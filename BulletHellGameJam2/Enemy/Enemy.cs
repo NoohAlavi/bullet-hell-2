dcb412aaf2313a7661e439280a1beda2c1fd8338
NoohAlavi <noohalavi@gmail.com>
2020-04-03 19:48:26 -0400

    Finish reworking scrolling

--- BulletHellGameJam2/Enemy/Enemy.cs
+++ BulletHellGameJam2/Enemy/Enemy.cs
@@ -3,8 +3,16 @@ using System;
 
 public class Enemy : KinematicBody2D
 {
+
+    public float Speed = 1f;
+
     public override void _Ready()
     {
 
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Position = new Vector2(Position.x, Position.y + Speed);
     }
 }
