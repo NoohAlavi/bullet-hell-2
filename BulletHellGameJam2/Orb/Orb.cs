7dcdd540e220c862bb361502597bb3c12ebc796a
NoohAlavi <noohalavi@gmail.com>
2020-04-04 18:40:56 -0400

    added orb

---
+++ BulletHellGameJam2/Orb/Orb.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class Orb : Area2D
+{
+    public override void _Ready()
+    {
+        Connect("body_entered", this, "OnBodyEntered");
+    }
+
+    public void OnBodyEntered(PhysicsBody2D body)
+    {
+        if (body is Player)
+        {
+            Player p = body as Player;
+        }
+    }
+}
